b3476dbf5a91737ca63c71550b3d6dcf22a71a88
Ccruz805 <63988491+Ccruz805@users.noreply.github.com>
2022-03-08 09:43:21 -0800

    Add files via upload

---
+++ Dessert-Page/script2.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#submit").click(function() {
+    window.location.reload();
+    alert("Thank you for contacting us, we will respond within 12 hours.");
+
+
+  });
+});
